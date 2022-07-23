fc9983fc4217a206374b2f3372ce1514d0dbc31c
Noorul Islam <41854212+noorul-islam@users.noreply.github.com>
2022-07-23 21:25:47 +0530

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -182,7 +182,7 @@
             </div>
           </div>
 
-          <footer className={styles.footer}>Made with &#10084; by LW3Punks</footer>
+          <footer className={styles.footer}>Made with &#10084; by Noorul Islam</footer>
         </div>
       );
     }
